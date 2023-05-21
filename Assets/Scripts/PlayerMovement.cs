8d182e1903c80c92253ffa0b2e506df2f56dacc3
gibbobobo <94616761+gibbobobo@users.noreply.github.com>
2023-05-21 14:55:27 +0100

    Added Boundary
    Added screen boundary so stop playing being able to move off screen.
    
    Also changed to horizontal screen.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,20 +7,34 @@ public class PlayerMovement : MonoBehaviour
 {
 
     Vector2 rawInput;
+    Vector2 minBound;
+    Vector2 maxBound;
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float padding = 1f;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetBounds();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 movement = rawInput * moveSpeed * Time.deltaTime;
-        transform.position += movement;
+        Vector2 movement = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + movement.x, minBound.x + padding, maxBound.x - padding);
+        newPos.y = Mathf.Clamp(transform.position.y + movement.y, minBound.y + padding, maxBound.y - padding);
+        transform.position = newPos;
+    }
+
+    void SetBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBound = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBound = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+
     }
 
     void OnMove(InputValue value)
