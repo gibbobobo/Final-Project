db88b9497797d987c2eaf3af82f089b5afcbcf4c
gibbobobo <94616761+gibbobobo@users.noreply.github.com>
2023-07-23 12:03:39 +0100

    First level prototype
    Created first level section from tileset.

---
+++ Assets/Scripts/Scroll.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+
+    [SerializeField] float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector2(moveSpeed * Time.deltaTime, 0);
+        transform.position -= movement;
+    }
+}
